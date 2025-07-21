a5af4b15fdc7a21f40d99d1ac75a7cfb4f4cbac5
Santy2206 <santiago2006171@gmail.com>
2025-07-21 09:58:30 -0500

    Add transformed title images and update event listeners for hover effects

--- javascript.js
+++ javascript.js
@@ -1,9 +1,28 @@
 eTitle=document.querySelector('[data-e]')
+tTitle=document.querySelector('[data-t]')
+cTitle=document.querySelector('[data-c]')
+hTitle=document.querySelector('[data-h]')
+middleBarTitle=document.querySelector('[data-middle-bar]')
+aTitle=document.querySelector('[data-a]')
+sTitle=document.querySelector('[data-s]')
+kTitle=document.querySelector('[data-k]')
+
+
 
   eTitle.addEventListener("mouseenter", () => {
-    eTitle.src = "Images/Transformed-Title/E.png"; // Change to new src on hover
+    eTitle.src = "Images/Transformed-Title/E.png"; 
   });
 
   eTitle.addEventListener("mouseleave", () => {
-    eTitle.src = "Images/Title/e.png"; // Revert back on hover out
+    eTitle.src = "Images/Title/e.png"; 
+    eTitle.style.transform="translateX(3px)"
+  });
+
+  tTitle.addEventListener("mouseenter", () => {
+    tTitle.src = "Images/Transformed-Title/T.png"; 
+  });
+
+  tTitle.addEventListener("mouseleave", () => {
+    tTitle.src = "Images/Title/t.png"; 
+    tTitle.style.transform="translateX(3px)"
   });
