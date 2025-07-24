23c77a315e18a48d142fa617bab1c56936765424
Santy2206 <santiago2006171@gmail.com>
2025-07-24 10:03:39 -0500

    Update button layout and enhance styling for improved user experience

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
-const letters=['e','t','c','h','middlebar','s','k',]
+const letters=['a','e','t','c','h','middlebar','s','k',]
 letters.forEach(letter=>{
   let selector=`[data-${letter}]`
   let elements=document.querySelectorAll(selector)
     elements.forEach(el => {
     el.addEventListener("mouseenter", () => {
-      // Capitalize for Transformed-Title
+  
       const capitalized = letter.toUpperCase();
       el.src = `Images/Transformed-Title/${capitalized}.png`;
     });
 
     el.addEventListener("mouseleave", () => {
-      // Lowercase for normal Title
+  
       const lowercased = letter.toLowerCase();
       el.src = `Images/Title/${lowercased}.png`;
       el.style.transform = "translateX(3px)";
