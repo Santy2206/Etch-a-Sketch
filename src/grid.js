0e0ab2ba5787fcad3befddb424ba63bfb4728fa4
Santy2206 <santiago2006171@gmail.com>
2025-10-08 14:27:57 -0500

    Remove unused grid functions and variables from grid.js

--- src/grid.js
+++ src/grid.js
@@ -1,34 +1,4 @@
-let isMouseDown = false;
-const backgroundColor = document.querySelector("[data-backgroundColor]");
-const pencelColor = document.querySelector("[data-pencelColor]");
-export function addGrid(container, size) {
-  const total = size * size;
-  for (let i = 0; i < total; i++) {
-    const div = document.createElement("div");
-    div.dataset.gridElement = "off";
-
-    div.addEventListener("mouseover", changeColor);
-    div.addEventListener("mousedown", (e) => {
-      e.target.style.background = pencelColor.value;
-    });
-    container.append(div);
-  }
-
-  container.style.gridTemplateColumns = `repeat(${size},1fr)`;
-  container.style.gridTemplateRows = `repeat(${size},1fr)`;
-}
-
-export function resetGrid() {
-  const gridElements = document.querySelectorAll("[data-grid-element]");
-  gridElements.forEach(element => element.style.background = backgroundColor.value);
-}
-export function setupGridEvents() {
-  document.addEventListener("mousedown", () => { isMouseDown = true; });
-  document.addEventListener("mouseup", () => { isMouseDown = false; });
-}
-
-function changeColor(e) {
-  if (isMouseDown) {
-    e.target.style.background =pencelColor.value ;
-  }
+function create_grid(times){
+  times=times*times
+  
 }
