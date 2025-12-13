e9e540ee3ae15b817b2079ed6c4e6e1d2daf96fe
Santy2206 <153763536+Santy2206@users.noreply.github.com>
2025-12-12 21:20:57 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -84,7 +84,7 @@ colorModesButtons.forEach((b)=>{
     if('rainbow' in b.dataset){
         const r = Math.floor(Math.random() * 256); // Random number between 0 and 255 for Red
         const g = Math.floor(Math.random() * 256); // Random number between 0 and 255 for Green
-        const b = Math.floor(Math.random() * 256); // Random number between 0 and 255 for Blue
+        const b = Math.floor(Math.random() * 256);
         newBackground=`rgb(${r}, ${g}, ${b});`
         gridElement.forEach(element=>{
           element.addEventListener("mouseover",e=>{
